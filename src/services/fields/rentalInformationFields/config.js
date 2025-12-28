9de2f00df5c2efb79c4f8481f37b248065b312f2
VikasRiskHawk <vikas.varma@riskhawk.net>
2025-12-28 18:53:34 +0530

    Add new textarea fields for rental demand details and investor-only demand factors
    - Introduced textarea fields for capturing additional details on rental demand and investor-only scenarios, enhancing data collection capabilities.
    - Updated the rental information fields configuration to include new mappings for these textarea inputs, ensuring accurate data extraction.
    - Enhanced the fields controller to process the new textarea fields, improving overall data handling and user input accuracy.

--- src/services/fields/rentalInformationFields/config.js
+++ src/services/fields/rentalInformationFields/config.js
@@ -1,7 +1,10 @@
 export const RENTAL_INFORMATION_FIELDS = [
     { key: 'in the locality?', source: 'checkbox', outputKey: 'isRentalDemandInLocality', yesLeft: 40.23, noLeft: 46.2 },
+    { key: 'If No, please provide details', source: 'textarea', outputKey: 'rentalDemandDetails', nextLabel: 'Monthly market rent sustainable assuming', textareaMapKey: 'rentalInformation_rentalDemandDetails' },
     { key: 'let on a 6/12 month AST basis, with the', source: 'valueCols', outputKey: 'monthlyMarketRentPresentCondition', targetLeft: 39.79 },
     { key: 'property in an improved condition and', labelIncludes: 'after any essential repairs', source: 'valueCols', outputKey: 'monthlyMarketRentImprovedCondition', targetLeft: 39.79},
     { key: 'on the ongoing demand for residential letting', source: 'checkbox', outputKey: 'isOtherLettingDemandFactors', yesLeft: 40.23, noLeft: 46.2},
+    { key: 'If Yes, please provide details', source: 'textarea', outputKey: 'otherLettingDemandDetails', nextLabel: 'Is the demand for this property only', textareaMapKey: 'rentalInformation_otherLettingDemandDetails' },
     { key: 'on an investor to investor basis?', source: 'checkbox', outputKey: 'investorOnlyDemand', yesLeft: 40.23, noLeft: 46.2 },
+    { key: 'If Yes, please provide details', source: 'textarea', outputKey: 'investorOnlyDemandDetails', nextLabel: 'GENERAL REMARKS:', textareaMapKey: 'rentalInformation_investorOnlyDemandDetails' },
 ]
